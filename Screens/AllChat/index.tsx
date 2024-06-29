dc7682a5a8c614569c9c64506eef41f219649b45
mohamad-ridwan <mr643062@gmail.com>
2024-06-29 07:33:57 +0700

    update

--- Screens/AllChat/index.tsx
+++ Screens/AllChat/index.tsx
@@ -1,6 +1,5 @@
 import { View, Text, Dimensions, StyleSheet, Modal, TextInput, ScrollView, RefreshControl, ActivityIndicator, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import PushNotification from "react-native-push-notification";
 import { useTheme } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { loginSessionName } from '@/utils/storage';
@@ -14,7 +13,6 @@ import { getChats } from '@/store/chat/chatAction';
 import { setJid, setLoader, setRecentChats } from '@/store/chat/chatSlice';
 import { getDevices } from '@/store/device/deviceAction';
 import { setDevice } from '@/store/device/deviceSlice';
-import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
 type Props = {
   navigation: any
@@ -45,55 +43,7 @@ export default function AllChatScreen({
 
   // NOTIFICATION CONFIGURE
   // Must be outside of any component LifeCycle (such as `componentDidMount`).
-  PushNotification.configure({
-    // (optional) Called when Token is generated (iOS and Android)
-    onRegister: function (token) {
-      console.log("TOKEN:", token);
-    },
-
-    // (required) Called when a remote is received or opened, or local notification is opened
-    onNotification: function (notification) {
-      console.log("NOTIFICATION:", notification);
-
-      // process the notification
-
-      // (required) Called when a remote is received or opened, or local notification is opened
-      notification.finish(PushNotificationIOS.FetchResult.NoData);
-    },
-
-    // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
-    onAction: function (notification) {
-      console.log("ACTION:", notification.action);
-      console.log("NOTIFICATION:", notification);
-
-      // process the action
-    },
-
-    // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
-    onRegistrationError: function (err) {
-      console.error(err.message, err);
-    },
-
-    // IOS ONLY (optional): default: all - Permissions to register.
-    permissions: {
-      alert: true,
-      badge: true,
-      sound: true,
-    },
-
-    // Should the initial notification be popped automatically
-    // default: true
-    popInitialNotification: true,
-
-    /**
-     * (optional) default: true
-     * - Specified if permissions (ios) and token (android and ios) will requested or not,
-     * - if not, you must call PushNotificationsHandler.requestPermissions() later
-     * - if you are not using remote notification or do not have Firebase installed, use this:
-     *     requestPermissions: Platform.OS === 'ios'
-     */
-    requestPermissions: true,
-  });
+  
   // --------NOTIFICATION CONFIGURE
 
   const error = ''
